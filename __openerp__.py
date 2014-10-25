6eacf169cd7e9eb2c50be9114a5625724deb17bd
cysnake4713 <cysnake4713@gmail.com>
2014-10-26 00:33:49 +0800

    Init V7

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Training Demo Module',
+    'version': '1.0',
+    'category': 'tr_demo',
+    'complexity': "easy",
+    'description': """
+Matt.Cai Training project demo.""",
+    'author': 'Matt Cai',
+    'website': 'http://odoosoft.com',
+    'depends': ['base'],
+    'data': [
+        'views/demo_menu.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
